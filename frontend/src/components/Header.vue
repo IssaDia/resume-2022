023575170fe18d20764961ae2c09adfacdd900f5
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-09-26 21:18:04 +0200

    put elements from header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="links">
+      <a href="#portfolio">Portfolio</a>
+      <a href="#mailto:issadiapro@gmail.com">contact</a>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.links {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
